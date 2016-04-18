8c27f2258d4a2db62d0a4f61d7e72a6dfb08f945
Agnieszka <krawczyk@mpimp-golm.mpg.de>
2016-04-18 16:07:42 +0200

    better code sanity check

--- pipeline/sanity.py
+++ pipeline/sanity.py
@@ -16,52 +16,15 @@ def check_sanity_data(filename):
         if 'genomes' in cp['GLOBAL']:
             genomes = cp['GLOBAL']['genomes'].split(';')
             # For each genome test that section
+            list_data = ['cds_fasta', 'genome_fasta', 'gff_file', 'gff_feature', 'gff_id', 'fastq_dir', 'bowtie_output', 'trimmomatic_output', 'tophat_output', 'samtools_output', 'htseq_output']
             for g in genomes:
+                if not all([i in cp[g].keys() for i in list_data]):
+                    print("missing data", file=sys.stderr)
 
-                if 'cds_fasta' not in cp[g]:
-                    print(g + " missing cds_fasta", file=sys.stderr)
-                    return False
-                if not os.path.exists(cp[g]['cds_fasta']):
-                    print(g + " cds_fasta doesn't exist", file=sys.stderr)
-                    return False
-                if 'genome_fasta' not in cp[g]:
-                    print(g + " missing genome_fasta", file=sys.stderr)
-                    return False
-                if not os.path.exists(cp[g]['genome_fasta']):
-                    print(g + " genome_fasta doesn't exist")
-                    return False
-                if 'gff_file' not in cp[g]:
-                    print(g + " missing gff_file", file=sys.stderr)
-                    return False
-                if not os.path.exists(cp[g]['gff_file']):
-                    print(g + " gff_file doesn't exist", file=sys.stderr)
-                    return False
-                if 'gff_feature' not in cp[g]:
-                    print(g + " missing gff_feature", file=sys.stderr)
-                    return False
-                if 'gff_id' not in cp[g]:
-                    print(g + " missing gff_id", file=sys.stderr)
-                    return False
-                if 'fastq_dir' not in cp[g]:
-                    print(g + " missing fastq_dir", file=sys.stderr)
-                    return False
-                if not os.path.exists(cp[g]['fastq_dir']):
-                    print(g + " fastq_dir doesn't exist", file=sys.stderr)
-                    return False
-                if 'bowtie_output' not in cp[g]:
-                    print(g + " missig bowtie_output", file=sys.stderr)
-                    return False
-                if 'trimmomatic_output' not in cp[g]:
-                    print(g + " missing trimmomatic_output", file=sys.stderr)
-                    return False
-                if 'tophat_output' not in cp[g]:
-                    print(g + " missing tophat_output", file=sys.stderr)
-                    return False
-                if 'samtools_output' not in cp[g]:
-                    print(g + " missing samtools_output", file=sys.stderr)
-                    return False
-                if 'htseq_output' not in cp[g]:
-                    print(g + " missing htseq_output", file=sys.stderr)
+                    for i in list_data:
+                        if i not in cp[g].keys():
+                            print("missing" + " " + i, file=sys.stderr)
+
                     return False
 
         else:
@@ -70,9 +33,6 @@ def check_sanity_data(filename):
     else:
         print("GLOBAL section missing", file=sys.stderr)
         return False
-
-
-
     return True
 
 
@@ -80,5 +40,17 @@ def check_sanity_config(filename):
     cp = configparser.ConfigParser()
     cp.read(filename)
 
+    list_tools = ['bowtie_module', 'samtools_module', 'sratoolkit_module', 'tophat_module', 'interproscan_module', 'blast_module', 'mcl_module', 'python_module', 'bowtie_cmd', 'trimmomatic_se_command', 'trimmomatic_pe_command', 'tophat_se_cmd', 'tophat_pe_cmd', 'samtools_cmd', 'htseq_count_cmd']
+    if 'TOOLS' in cp:
+        if not all(k in cp['TOOLS'].keys() for k in list_tools):
+            print("missing tool", file=sys.stderr)
+
+            for k in list_tools:
+                if k not in cp['TOOLS'].keys():
+                    print("missing" + " " + k, file=sys.stderr)
+            return False
 
     return True
+
+
+
