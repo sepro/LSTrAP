eb8bf69131d7ea1cc2a25a820dea0cf05ed86148
sepro <sebastian.proost@gmail.com>
2016-06-17 13:34:03 +0200

    added missing sanity check

--- pipeline/check/sanity.py
+++ pipeline/check/sanity.py
@@ -63,7 +63,7 @@ def check_sanity_config(filename):
     required_keys = ['bowtie_module', 'samtools_module', 'sratoolkit_module', 'tophat_module', 'interproscan_module',
                      'blast_module', 'mcl_module', 'python_module', 'python3_module', 'bowtie_cmd', 'trimmomatic_se_command',
                      'trimmomatic_pe_command', 'tophat_se_cmd', 'tophat_pe_cmd', 'samtools_cmd', 'htseq_count_cmd',
-                     'interproscan_cmd', 'pcc_cmd', 'mcl_cmd', 'orthofinder_cmd', 'orthofinder_cores']
+                     'interproscan_cmd', 'pcc_cmd', 'mcl_cmd', 'orthofinder_cmd', 'orthofinder_cores', 'mcxdeblast_cmd']
 
     if 'TOOLS' in cp:
         if not all(k in cp['TOOLS'].keys() for k in required_keys):
