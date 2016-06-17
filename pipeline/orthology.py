5bc683cefc41622c5f0498fe039ab656dfbd570e
sepro <sebastian.proost@gmail.com>
2016-06-17 14:58:19 +0200

    typo fixed (yet another)

--- pipeline/orthology.py
+++ pipeline/orthology.py
@@ -95,7 +95,7 @@ class OrthologyPipeline(PipelineBase):
         with open(mcl_families_out, 'r') as infile, open(os.path.join(orthofinder_dir, 'mcl_families.processed.txt'), 'w') as outfile:
             for l in infile:
                 parts = [id_conversion[id] if id in id_conversion.keys() else '!error!' for id in l.strip().split()]
-                print('\t'.join(parts), outfile)
+                print('\t'.join(parts), file=outfile)
 
         # remove the submission script
         os.remove(filename)
