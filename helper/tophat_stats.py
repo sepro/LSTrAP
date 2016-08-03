8a580f0556b153228dc9029c3f7d03a16b161a21
sepro <sebastian.proost@gmail.com>
2016-08-03 10:18:12 +0200

    Added script to pull tophat stats from output

---
+++ helper/tophat_stats.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+Script to iterate over TopHat output and grab quality statistics
+"""
+import os
+import re
+
+from sys import argv
+from collections import defaultdict
+
+tophat_path = argv[1]
+
+values = defaultdict(list)
+
+# Prepare regex
+re_mapped = re.compile('Mapped   :\s+\d+ \( (\d+\.*\d*) of input\)')
+
+# Get all directories in this path
+for d in os.listdir(path=tophat_path):
+
+    # Only consider directories
+    if os.path.isdir(d):
+        summary = os.path.join(tophat_path, d, 'align_summary.txt')
+
+        # Check if summary exisists
+        if os.path.exists(summary):
+            # process summary file
+            with open(summary) as f:
+                lines = '\t'.join(f.readlines())
+                hits = re_mapped.search(lines)
+                print(hits[1])
