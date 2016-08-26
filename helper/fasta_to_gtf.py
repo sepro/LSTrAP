14f84b56fce33d902ed8bf395ac7a6a523cd6b20
sepro <sebastian.proost@gmail.com>
2016-08-26 14:38:32 +0200

    fasta_to_gtf.py

--- helper/fasta_to_gtf.py
+++ helper/fasta_to_gtf.py
@@ -7,7 +7,4 @@ fasta_file = sys.argv[1]
 fasta_data = Fasta.readfile(fasta_file)
 
 for k, s in fasta_data.sequences.items():
-
-    feature = ''
-
     print('%s\t.\tCDS\t1\t%d\t.\t.\t.\tgene_id "Parent=%s"\n' % (k, len(s), k))
